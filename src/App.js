81783ca348872e1902d6677b40b03b874aaf806f
James-Kelley-Galvanize <james.kelley@galvanize.com>
2022-02-10 09:23:27 -0700

    simulte an api call during componentDidMount being used to set pokeData to state

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      pokeData,
+      pokeData: [], // allows the app to start without pokeData in the state because you can map over an empty array
       selectedPokemon: false,
     };
     this.selectPokemon = this.selectPokemon.bind(this);
   }
 
+  componentDidMount() {
+    // this is where you make a fetch call and make your pokeData array
+    this.setState({ pokeData }); // this simulates setting the state with data returned from a fetch call
+  }
+
   selectPokemon(index) {
-    // console.log(`pokemon index:`, index);
     let isAlreadyActive = index === this.state.selectedPokemon;
     this.setState({ selectedPokemon: isAlreadyActive ? false : index });
   }
@@ -23,15 +27,16 @@ class App extends Component {
   // REMINDER - TALK ABOUT FETCH AND COMPONENTDIDMOUNT AT THE END
   render() {
     let { pokeData, selectedPokemon } = this.state;
-
     return (
       <div className="App">
         <header className="App-header">
-          <PokemonList
-            pokeData={pokeData}
-            selectPokemon={this.selectPokemon}
-            selectedPokemon={selectedPokemon}
-          />
+          {
+            <PokemonList
+              pokeData={pokeData} // This must be an array for the app to spin up without crashing
+              selectPokemon={this.selectPokemon}
+              selectedPokemon={selectedPokemon}
+            />
+          }
         </header>
       </div>
     );
